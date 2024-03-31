bf41f2ea8b2a2908bc1359291b48ca13af0f41a0
Kirill Temnenkov <kirill@temnenkov.com>
2024-03-31 16:05:42 +0300

    test4 ok

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+enum State {
+    NORMAL, WAIT_NAME, RECORD_DEF
+}
+
 class Solution {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         if (in.hasNextLine()) {
@@ -17,129 +24,164 @@ class Solution {
             String line = in.nextLine();
             interpreter.exec(line);
         }
-
-        // Write an answer using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-       // System.out.println("answer");
     }
 }
 
 class Interpreter {
 
     final Deque<Integer> stack = new LinkedList<>();
+    final Map<String, List<String>> defs = new HashMap<>();
+
+    String currentDefName;
+
+    State state = State.NORMAL;
 
     void exec(String s) {
-        final String[] tokens = s.split(" ");
-        for(String token : tokens) {
-            final Integer number = tryParse(token);
-            if (number != null) {
-                stack.push(number);
-                continue;
-            }
+        final List<String> tokens = Arrays.stream(s.split(" ")).toList();
+        exec(tokens);
+    }
 
-            if ("ADD".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg0 + arg1);
-                continue;
-            }
+    private void exec(List<String> tokens) {
+        for (String token : tokens) {
 
-            if ("SUB".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg1 - arg0);
-                continue;
-            }
-
-            if ("MUL".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg0 * arg1);
-                continue;
-            }
-
-            if ("DIV".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg1 / arg0);
-                continue;
-            }
-
-            if ("MOD".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg1 % arg0);
-                continue;
-            }
-
-            if ("OUT".equals(token)) {
-                final Integer arg0 = stack.pop();
-                System.out.println(arg0);
-                continue;
-            }
-
-            if ("POP".equals(token)) {
-                stack.pop();
-                continue;
-            }
-
-            if ("SWP".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg0);
-                stack.push(arg1);
-                continue;
-            }
-
-            if ("DUP".equals(token)) {
-                final Integer arg0 = stack.pop();
-                stack.push(arg0);
-                stack.push(arg0);
-                continue;
-            }
-
-            if ("ROT".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                final Integer arg2 = stack.pop();
-                stack.push(arg1);
-                stack.push(arg0);
-                stack.push(arg2);
-                continue;
-            }
-
-            if ("OVR".equals(token)) {
-                final Integer arg0 = stack.pop();
-                final Integer arg1 = stack.pop();
-                stack.push(arg1);
-                stack.push(arg0);
-                stack.push(arg1);
-                continue;
-            }
-
-            if ("NOT".equals(token)) {
-                final Integer arg0 = stack.pop();
-                if (0 == arg0) {
-                    stack.push(1);
-                } else {
-                    stack.push(0);
+            switch (state) {
+                case NORMAL -> execNormalToken(token);
+                case WAIT_NAME -> {
+                    currentDefName = token;
+                    defs.put(currentDefName, new ArrayList<>());
+                    state = State.RECORD_DEF;
                 }
-                continue;
-            }
-
-            if ("POS".equals(token)) {
-                final Integer arg0 = stack.pop();
-                if (arg0 >= 0) {
-                    stack.push(1);
-                } else {
-                    stack.push(0);
+                case RECORD_DEF -> {
+                    if ("END".equals(token)) {
+                        state = State.NORMAL;
+                    } else {
+                        defs.get(currentDefName).add(token);
+                    }
                 }
-                continue;
             }
 
-            throw new IllegalArgumentException("Unexpected '" + token + "'");
         }
+    }
+
+    private void execNormalToken(String token) {
+        final Integer number = tryParse(token);
+        if (number != null) {
+            stack.push(number);
+            return;
+        }
+
+        final List<String> customDef = defs.get(token);
+        if (customDef != null) {
+            exec(customDef);
+            return;
+        }
+
+        if ("DEF".equals(token)) {
+            state = State.WAIT_NAME;
+            return;
+        }
+
+        if ("ADD".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg0 + arg1);
+            return;
+        }
+
+        if ("SUB".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg1 - arg0);
+            return;
+        }
+
+        if ("MUL".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg0 * arg1);
+            return;
+        }
+
+        if ("DIV".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg1 / arg0);
+            return;
+        }
+
+        if ("MOD".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg1 % arg0);
+            return;
+        }
+
+        if ("OUT".equals(token)) {
+            final Integer arg0 = stack.pop();
+            System.out.println(arg0);
+            return;
+        }
+
+        if ("POP".equals(token)) {
+            stack.pop();
+            return;
+        }
+
+        if ("SWP".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg0);
+            stack.push(arg1);
+            return;
+        }
+
+        if ("DUP".equals(token)) {
+            final Integer arg0 = stack.pop();
+            stack.push(arg0);
+            stack.push(arg0);
+            return;
+        }
+
+        if ("ROT".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            final Integer arg2 = stack.pop();
+            stack.push(arg1);
+            stack.push(arg0);
+            stack.push(arg2);
+            return;
+        }
+
+        if ("OVR".equals(token)) {
+            final Integer arg0 = stack.pop();
+            final Integer arg1 = stack.pop();
+            stack.push(arg1);
+            stack.push(arg0);
+            stack.push(arg1);
+            return;
+        }
+
+        if ("NOT".equals(token)) {
+            final Integer arg0 = stack.pop();
+            if (0 == arg0) {
+                stack.push(1);
+            } else {
+                stack.push(0);
+            }
+            return;
+        }
+
+        if ("POS".equals(token)) {
+            final Integer arg0 = stack.pop();
+            if (arg0 >= 0) {
+                stack.push(1);
+            } else {
+                stack.push(0);
+            }
+            return;
+        }
+
+        throw new IllegalArgumentException("Unexpected '" + token + "'");
     }
 
     private Integer tryParse(String token) {
