5793b1b32283188878ab5b7cf2b33dd875c1f48c
Kirill Temnenkov <kirill@temnenkov.com>
2024-03-31 12:44:35 +0300

    example test ok

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,7 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.*;
-import java.io.*;
-import java.math.*;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -10,17 +11,88 @@ class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        int n = in.nextInt();
         if (in.hasNextLine()) {
             in.nextLine();
         }
-        for (int i = 0; i < N; i++) {
+        final Interpreter interpreter = new Interpreter();
+        for (int i = 0; i < n; i++) {
             String line = in.nextLine();
+            interpreter.exec(line);
         }
 
         // Write an answer using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("answer");
+       // System.out.println("answer");
     }
+}
+
+class Interpreter {
+
+    final Deque<Integer> stack = new LinkedList<>();
+
+    void exec(@NotNull String s) {
+        final String[] tokens = s.split(" ");
+        for(String token : tokens) {
+            final Integer number = tryParse(token);
+            if (number != null) {
+                stack.push(number);
+                continue;
+            }
+
+            if ("ADD".equals(token)) {
+                final Integer arg0 = stack.pop();
+                final Integer arg1 = stack.pop();
+                stack.push(arg0 + arg1);
+                continue;
+            }
+
+            if ("SUB".equals(token)) {
+                final Integer arg0 = stack.pop();
+                final Integer arg1 = stack.pop();
+                stack.push(arg1 - arg0);
+                continue;
+            }
+
+            if ("MUL".equals(token)) {
+                final Integer arg0 = stack.pop();
+                final Integer arg1 = stack.pop();
+                stack.push(arg0 * arg1);
+                continue;
+            }
+
+            if ("DIV".equals(token)) {
+                final Integer arg0 = stack.pop();
+                final Integer arg1 = stack.pop();
+                stack.push(arg1 / arg0);
+                continue;
+            }
+
+            if ("MOD".equals(token)) {
+                final Integer arg0 = stack.pop();
+                final Integer arg1 = stack.pop();
+                stack.push(arg1 % arg0);
+                continue;
+            }
+
+            if ("OUT".equals(token)) {
+                final Integer arg0 = stack.pop();
+                System.out.println(arg0);
+                continue;
+            }
+        }
+    }
+
+    @Nullable Integer tryParse(@NotNull String token) {
+        Integer result;
+        try {
+            result = Integer.valueOf(token);
+        } catch (NumberFormatException e) {
+            result = null;
+        }
+        return result;
+    }
+
+
 }
